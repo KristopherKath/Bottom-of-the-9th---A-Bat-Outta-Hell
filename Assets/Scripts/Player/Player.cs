cb686e4bca338a97444c70ee2d46a6e64e1223a5
Kristopher Kath <Kristopher.Kath@gmail.com>
2022-02-02 11:57:14 -0800

    Added input Maps

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int startHealth;
+    int health;
+    public float bulletCooldown;
+    float bulletTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = startHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bulletTimer -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet" && bulletTimer <=0)
+        {
+            health -= 1;
+            print(health);
+            bulletTimer = bulletCooldown;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
     }
 }
